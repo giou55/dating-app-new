fe21f2a68677c6877418ec1356d1bc2a84ef8e8f
giou55 <giourmetakis00@gmail.com>
2023-01-28 14:32:17 +0200

    changes

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -12,6 +12,11 @@ import { AdminService } from 'src/app/_services/admin.service';
 export class UserManagementComponent implements OnInit {
   users: User[] = [];
   bsModalRef: BsModalRef<RolesModalComponent> = new BsModalRef<RolesModalComponent>();
+  availableRoles = [
+    'Admin',
+    'Moderator',
+    'Member'
+  ];
 
   constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
@@ -25,19 +30,34 @@ export class UserManagementComponent implements OnInit {
     })
   }
 
-  openRolesModal() {
-    const initialState: ModalOptions = {
+  openRolesModal(user: User) {
+    const config = {
+      class: 'modal-dialog-centered',
       initialState: {
-        list: [
-          'Do thing',
-          'Pass your data',
-          'Do something else'
-        ],
-        title: 'test modal'
+        username: user.username,
+        availableRoles: this.availableRoles,
+        selectedRoles: [...user.roles]
       }
     };
-    this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
-    this.bsModalRef.content!.closeBtnName = 'Close';
+    this.bsModalRef = this.modalService.show(RolesModalComponent, config);
+    this.bsModalRef.onHide?.subscribe({
+      // when the modal is hidden, we only want to do something if 
+      // the user has actually updated the roles,
+      // so we need to check for equality of two arrays
+      // before call adminService method
+      next: () => {
+        const selectedRoles = this.bsModalRef.content?.selectedRoles;
+        if (!this.arrayEqual(selectedRoles!, user.roles)) {
+          this.adminService.updateUserRoles(user.username, selectedRoles).subscribe({
+            next: roles => user.roles = roles
+          })
+        }
+      }
+    })
+  }
+
+  private arrayEqual(arr1: any[], arr2: any[]) {
+    return JSON.stringify(arr1.sort()) === JSON.stringify(arr2.sort());
   }
 
 }
--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -9,13 +9,25 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 export class RolesModalComponent implements OnInit {
   // these properties is populated from openRolesModal method
   // inside UserManagementComponent class
-  title = '';
-  list: any;
-  closeBtnName = '';
+  username = '';
+  availableRoles: any[] = [];
+  selectedRoles: any[] = [];
 
   constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
+  }
+
+  // this method change the values of whether a checkbox is checked or not checked
+  // based on which role the user is in
+  updateChecked(checkedValue: string) {
+    // if the index is equal to -1, that means it's not inside the selected roles array,
+    // and if it's not inside the selected roles array and it's been checked,
+    // then we want to add it to the selected roles array,
+    // otherwise we want to remove it
+    const index = this.selectedRoles.indexOf(checkedValue);
+    index !== -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkedValue);
+
   }
 
 }
