571512104a2c1cb41eea2644198f5efc28f99ce9
giou55 <giourmetakis00@gmail.com>
2023-02-06 14:31:59 +0200

    changes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,15 +11,16 @@ export class NavComponent implements OnInit {
   model: any = {}
 
   constructor(
-    public accountService: AccountService, 
-    private router: Router,
-    private toastr: ToastrService) {}
+    public accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {}
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: () => this.router.navigateByUrl('/members')
+      next: () => {
+        this.router.navigateByUrl('/members');
+        this.model = {}; // when user logout, the fields of the form will be empty
+      }
     })
   }
 
