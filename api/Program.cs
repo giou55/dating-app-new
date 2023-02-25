df0ae348da8ad1bc2bd55b9598296a44f4a54cd5
giou55 <giourmetakis00@gmail.com>
2023-02-25 22:15:52 +0200

    changes

--- api/Program.cs
+++ api/Program.cs
@@ -18,33 +18,33 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 
 // we add this code for deploying our app to fly.io
-// var connString = "";
+var connString = "";
 
-// if (builder.Environment.IsDevelopment()) 
-//     connString = builder.Configuration.GetConnectionString("DefaultConnection");
-// else 
-// {
-// // Use connection string provided at runtime by Fly.io.
-//     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (builder.Environment.IsDevelopment()) 
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else 
+{
+// Use connection string provided at runtime by Fly.io.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-//     // Parse connection URL to connection string for Npgsql
-//     connUrl = connUrl.Replace("postgres://", string.Empty);
-//     var pgUserPass = connUrl.Split("@")[0];
-//     var pgHostPortDb = connUrl.Split("@")[1];
-//     var pgHostPort = pgHostPortDb.Split("/")[0];
-//     var pgDb = pgHostPortDb.Split("/")[1];
-//     var pgUser = pgUserPass.Split(":")[0];
-//     var pgPass = pgUserPass.Split(":")[1];
-//     var pgHost = pgHostPort.Split(":")[0];
-//     var pgPort = pgHostPort.Split(":")[1];
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
 
-//     connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-// }
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
 
-// builder.Services.AddDbContext<DataContext>(opt =>
-// {
-//     opt.UseNpgsql(connString);
-// });
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+});
 // end of code for deploy to fly.io
 
 
@@ -81,7 +81,7 @@ app.MapHub<MessageHub>("hubs/message");
 
 // we specify the action and the fallback controller
 // when our API doesn't know how to handle a specific route
-//app.MapFallbackToController("Index", "Fallback");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
